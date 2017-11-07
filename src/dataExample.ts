e45ca425ac411b7f4f455f8082a7ab2d8aeaa36b
Petrisor Lacatus <placatus@ptc.com>
2017-11-07 13:15:51 +0200

    Added datamodel Example

---
+++ src/dataExample.ts
@@ -0,0 +1,400 @@
+let dataModel = {
+    suppliers: [
+        {
+            name: "Industrialesud GmbH / Landau / DE",
+            details: "",
+            id: '1',
+            parts: [
+                {
+                    'id': '2',
+                    'PlantName': 'Industrialesud GmbH',
+                    'Lft-Nr+ZA': '151573-12',
+                    'Ort / Land': 'Landau / DE',
+                    'Produktion': undefined,
+                    'Sequenzprod.': 'ja',
+                    'Teilefamilie': undefined,
+                    'Name': 'Himmel',
+                    'Variantenzahl': 'G30/F90: 18; F34/F36: 132',
+                    'BehälterTyp': '3104026',
+                    'Füllgrad': '6',
+                    'needBy': 'bis 06/20'
+                },
+                {
+                    'id': '3',
+                    'PlantName': 'Industrialesud GmbH',
+                    'Lft-Nr+ZA': '151573-12',
+                    'Ort / Land': 'Landau / DE',
+                    'Produktion': undefined,
+                    'Sequenzprod.': 'ja',
+                    'Teilefamilie': undefined,
+                    'Name': 'Himmel',
+                    'Variantenzahl': 'G30/F90: 18; F34/F36: 132',
+                    'BehälterTyp': '3104026',
+                    'Füllgrad': '6',
+                    'needBy': 'bis 08/20'
+                },
+                {
+                    'id': '4',
+                    'PlantName': 'Industrialesud GmbH',
+                    'Lft-Nr+ZA': '151573-12',
+                    'Ort / Land': 'Landau / DE',
+                    'Produktion': undefined,
+                    'Sequenzprod.': 'ja',
+                    'Teilefamilie': undefined,
+                    'Name': 'Himmel',
+                    'Variantenzahl': 'G30/F90: 18; F34/F36: 132',
+                    'BehälterTyp': '3104026',
+                    'Füllgrad': '6',
+                    'needBy': 'bis 08/20'
+                },
+                {
+                    'id': '5',
+                    'PlantName': 'Industrialesud GmbH',
+                    'Lft-Nr+ZA': '151573-12',
+                    'Ort / Land': 'Landau / DE',
+                    'Produktion': undefined,
+                    'Sequenzprod.': 'ja',
+                    'Teilefamilie': undefined,
+                    'Name': 'Himmel',
+                    'Variantenzahl': 'G30/F90: 18; F34/F36: 132',
+                    'BehälterTyp': '3104026',
+                    'Füllgrad': '6',
+                    'needBy': 'bis 08/20'
+                },
+                {
+                    'id': '6',
+                    'PlantName': 'Industrialesud GmbH',
+                    'Lft-Nr+ZA': '151573-12',
+                    'Ort / Land': 'Landau / DE',
+                    'Produktion': undefined,
+                    'Sequenzprod.': 'ja',
+                    'Teilefamilie': undefined,
+                    'Name': 'Himmel',
+                    'Variantenzahl': 'G30/F90: 18; F34/F36: 132',
+                    'BehälterTyp': '3104026',
+                    'Füllgrad': '6',
+                    'needBy': 'bis 08/20'
+                }
+            ]
+        },
+        {
+            name: "Grupo Antolin Bohema A.S.  / Liberec / CZ / G31/ G32",
+            details: "",
+            'id': '7',
+            parts: [
+                {
+                    'id': '8',
+                    'PlantName': 'Grupo Antolin Bohema A.S.',
+                    'Lft-Nr+ZA': '151573-12',
+                    'Ort / Land': 'Landau / DE',
+                    'Produktion': undefined,
+                    'Sequenzprod.': 'ja',
+                    'Teilefamilie': undefined,
+                    'Name': 'Himmel',
+                    'Variantenzahl': 'G30/F90: 18; F34/F36: 132',
+                    'BehälterTyp': '3104026',
+                    'Füllgrad': '6',
+                    'needBy': 'bis 06/20'
+                }
+            ]
+        },
+        {
+            name: "IACG S.R.O. / Prestice / CZ / G11/G12, F06",
+            details: "",
+            'id': '9',
+            parts: [
+                {
+                    'id': '10',
+                    'PlantName': 'Industrialesud GmbH',
+                    'Lft-Nr+ZA': '151573-12',
+                    'Ort / Land': 'Landau / DE',
+                    'Produktion': undefined,
+                    'Sequenzprod.': 'ja',
+                    'Teilefamilie': undefined,
+                    'Name': 'Himmel',
+                    'Variantenzahl': 'G30/F90: 18; F34/F36: 132',
+                    'BehälterTyp': '3104026',
+                    'Füllgrad': '6',
+                    'needBy': 'bis 06/20'
+                },
+                {
+                    'id': '11',
+                    'PlantName': 'Industrialesud GmbH',
+                    'Lft-Nr+ZA': '151573-12',
+                    'Ort / Land': 'Landau / DE',
+                    'Produktion': undefined,
+                    'Sequenzprod.': 'ja',
+                    'Teilefamilie': undefined,
+                    'Name': 'Himmel',
+                    'Variantenzahl': 'G30/F90: 18; F34/F36: 132',
+                    'BehälterTyp': '3104026',
+                    'Füllgrad': '6',
+                    'needBy': 'bis 08/20'
+                }
+            ]
+        },
+        {
+            name: "Holzwastl HandelsgmbH / Eisenstadt / AT / F13 ",
+            details: "",
+            'id': '12',
+            parts: [
+                {
+                    'id': '13',
+                    'PlantName': 'Industrialesud GmbH',
+                    'Lft-Nr+ZA': '151573-12',
+                    'Ort / Land': 'Landau / DE',
+                    'Produktion': undefined,
+                    'Sequenzprod.': 'ja',
+                    'Teilefamilie': undefined,
+                    'Name': 'Himmel',
+                    'Variantenzahl': 'G30/F90: 18; F34/F36: 132',
+                    'BehälterTyp': '3104026',
+                    'Füllgrad': '6',
+                    'needBy': 'bis 06/20'
+                }
+            ]
+        },
+        {
+            name: "AWF KFT (Holzwastl)/Sopron/HU/G15/G16/F92/F93",
+            details: "",
+            'id': '14',
+            parts: [
+                {
+                    'id': '15',
+                    'PlantName': 'Industrialesud GmbH',
+                    'Lft-Nr+ZA': '151573-12',
+                    'Ort / Land': 'Landau / DE',
+                    'Produktion': undefined,
+                    'Sequenzprod.': 'ja',
+                    'Teilefamilie': undefined,
+                    'Name': 'Himmel',
+                    'Variantenzahl': 'G30/F90: 18; F34/F36: 132',
+                    'BehälterTyp': '3104026',
+                    'Füllgrad': '6',
+                    'needBy': 'bis 06/20'
+                }
+            ]
+        }
+    ],
+    logisticsCenters: [
+        {
+            name: " VZ3 Kühne & Nagel",
+            details: "",
+            'id': '16',
+            capabilities: [
+                {
+                    type: 'forklift'
+                },
+                {
+                    type: 'process',
+                    name: 'Sequenzierung F13/06',
+                    capabilities: [{ type: 'operator' }, { type: 'X' }]
+                },
+                {
+                    'type': 'part',
+                    'Teilefamilie': 'Himmel F13/F06',
+                    'Behälter': '3086699',
+                    'Füllgrad': '11'
+                },
+                {
+                    type: 'forklift'
+                }
+            ]
+        }
+    ],
+    factory: {
+        name: "BMW Werk 02.40",
+        'id': '17',
+        halls: [
+            {
+                name: 'Halle 50',
+                'id': '18',
+                inventories: [
+                    {
+                        'id': '19',
+                        name: 'G30/F34/F36',
+                        details: 'bis 06/20',
+                        info: {
+                            'Vorlaufbestand [h]': '5,4',
+                            'Anzahl Behälter': '191',
+                            'Fläche [m²]': 'schwankend',
+                            'UT': '1'
+                        }
+                    },
+                    {
+                        'id': '20',
+                        name: 'G30/F34/F36',
+                        details: 'bis 06/20',
+                        info: {
+                            'Vorlaufbestand [h]': '5,4',
+                            'Anzahl Behälter': '191',
+                            'Fläche [m²]': 'schwankend',
+                            'UT': '1'
+                        }
+                    },
+                    {
+                        'id': '21',
+                        name: 'G30/F34/F36',
+                        details: 'bis 06/20',
+                        info: {
+                            'Vorlaufbestand [h]': '5,4',
+                            'Anzahl Behälter': '191',
+                            'Fläche [m²]': 'schwankend',
+                            'UT': '1'
+                        }
+                    },
+                    {
+                        'id': '22',
+                        name: 'G30/F34/F36',
+                        details: 'bis 06/20',
+                        info: {
+                            'Vorlaufbestand [h]': '5,4',
+                            'Anzahl Behälter': '191',
+                            'Fläche [m²]': 'schwankend',
+                            'UT': '1'
+                        }
+                    }
+                ],
+                capabilities: [
+                    {
+                        type: 'lift',
+                        name: 'Aufzuf 7'
+                    },
+                    {
+                        type: 'process',
+                        name: 'VoMo 50 1 60 013',
+                        capabilities: [{ type: 'operator' }, { type: 'X' }]
+                    },
+                    {
+                        type: 'process',
+                        name: '50 1 60 013',
+                        capabilities: [{ type: 'text', value: 'F34/F36/G30/ G22/G82 G31/G32' }]
+                    },
+                ]
+            },
+            {
+                name: 'Halle 51',
+                'id': '23',
+                inventories: [
+                    {
+                        'id': '24',
+                        name: 'G30/F34/F36',
+                        details: 'bis 06/20',
+                        info: {
+                            'Vorlaufbestand [h]': '5,4',
+                            'Anzahl Behälter': '191',
+                            'Fläche [m²]': 'schwankend',
+                            'UT': '1'
+                        }
+                    },
+                    {
+                        'id': '25',
+                        name: 'G30/F34/F36',
+                        details: 'bis 06/20',
+                        info: {
+                            'Vorlaufbestand [h]': '5,4',
+                            'Anzahl Behälter': '191',
+                            'Fläche [m²]': 'schwankend',
+                            'UT': '1'
+                        }
+                    },
+                    {
+                        'id': '26',
+                        name: 'G30/F34/F36',
+                        details: 'bis 06/20',
+                        info: {
+                            'Vorlaufbestand [h]': '5,4',
+                            'Anzahl Behälter': '191',
+                            'Fläche [m²]': 'schwankend',
+                            'UT': '1'
+                        }
+                    },
+                    {
+                        'id': '26',
+                        name: 'G30/F34/F36',
+                        details: 'bis 06/20',
+                        info: {
+                            'Vorlaufbestand [h]': '5,4',
+                            'Anzahl Behälter': '191',
+                            'Fläche [m²]': 'schwankend',
+                            'UT': '1'
+                        }
+                    }
+                ],
+                capabilities: [
+                    {
+                        type: 'lift',
+                        name: 'Aufzuf 7'
+                    },
+                    {
+                        type: 'process',
+                        name: 'VoMo 50 1 60 013',
+                        capabilities: [{ type: 'operator' }, { type: 'X' }]
+                    },
+                    {
+                        type: 'process',
+                        name: '50 1 60 013',
+                        capabilities: [{ type: 'text', value: 'F34/F36/G30/ G22/G82 G31/G32' }]
+                    },
+                ]
+            }
+        ]
+    },
+    transportLinks: [{
+        fromId: '1',
+        toId: '5',
+        type: 'truck',
+        info: {
+            'Aktuell': '1 x pro Tag',
+            'Entfernung': '160 km',
+            'Fahrzeit': '3 h'
+        },
+        label: 'F06'
+    },
+    {
+        fromId: '8',
+        toId: '13',
+        type: 'truck',
+        info: {
+            'Aktuell': '1 x pro Tag',
+            'Entfernung': '160 km',
+            'Fahrzeit': '3 h'
+        },
+        label: 'F06'
+    },
+    {
+        fromId: '13',
+        toId: '21',
+        type: 'truck',
+        info: {
+            'Aktuell': '1 x pro Tag',
+            'Entfernung': '160 km',
+            'Fahrzeit': '3 h'
+        },
+        label: 'F06'
+    }, {
+        fromId: '21',
+        toId: '14',
+        type: 'truck',
+        info: {
+            'Aktuell': '1 x pro Tag',
+            'Entfernung': '160 km',
+            'Fahrzeit': '3 h'
+        },
+        label: 'F06'
+    },
+    {
+        fromId: '13',
+        toId: '19',
+        type: 'truck',
+        info: {
+            'Aktuell': '1 x pro Tag',
+            'Entfernung': '160 km',
+            'Fahrzeit': '3 h'
+        },
+        label: 'F06'
+    }
+    ]
+}
+
+export = dataModel;
