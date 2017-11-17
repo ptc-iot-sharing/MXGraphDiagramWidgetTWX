b0001de4f23e1e536b7a30140029f0a82c5493d3
Petrisor Lacatus <placatus@ptc.com>
2017-11-17 11:36:17 +0200

    Enable the display of the data loading

--- src/mxdiagram.runtime.ts
+++ src/mxdiagram.runtime.ts
@@ -1,34 +1,40 @@
-TW.Runtime.Widgets.mxdiagram = function() {
+TW.Runtime.Widgets.mxdiagram = function () {
     let valueProcessDiagramLoader, mxGraphUtils;
     // a list of resources that are hold by the current graph
     let currentGraphResources = [];
     // the html is really simple. just a div acting as the container
-    this.renderHtml = function() {
+    this.renderHtml = function () {
         return '<div class="widget-content widget-mxgraph"></div>';
     };
 
+    this.runtimeProperties  =  function  () {
+        return  {
+            needsDataLoadingAndError: true,
+        };
+    }
+
     this.afterRender = async function () {
         valueProcessDiagramLoader = await import('./value_process/mxValueProcessDiagram');
-        if(this.getProperty('ShowTools') || this.getProperty('ShowOutline')) {
-            mxGraphUtils = await import('./value_process/mxGraphUtils');   
+        if (this.getProperty('ShowTools') || this.getProperty('ShowOutline')) {
+            mxGraphUtils = await import('./value_process/mxGraphUtils');
         }
     }
 
     this.updateProperty = async function (updatePropertyInfo) {
         this.setProperty(updatePropertyInfo.TargetProperty, updatePropertyInfo.RawDataFromInvoke);
         switch (updatePropertyInfo.TargetProperty) {
-            case 'ValueDiagram': 
+            case 'ValueDiagram':
                 this.resetCurrentGraph();
                 let container = this.jqElement[0];
                 let currentGraph = valueProcessDiagramLoader.createValueProcessDiagram(container, updatePropertyInfo.RawDataFromInvoke);
-                if(this.getProperty('ShowTools')) {
+                if (this.getProperty('ShowTools')) {
                     currentGraphResources.push(mxGraphUtils.CreateGraphToolbar(currentGraph));
-                } 
-                if(this.getProperty('ShowTools')) {
+                }
+                if (this.getProperty('ShowTools')) {
                     currentGraphResources.push(mxGraphUtils.CreateGraphOutline(currentGraph));
                 }
                 currentGraphResources.push(currentGraph);
-            break;
+                break;
         }
     }
 
