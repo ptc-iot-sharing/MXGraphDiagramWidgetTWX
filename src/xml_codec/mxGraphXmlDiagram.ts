ca6a3db3dea7ee0c6bb1c99e3081aec83ae88e0f
Petrisor Lacatus <placatus@ptc.com>
2017-11-23 16:24:45 +0200

    Remove the mxcodec overrides from the xml import

--- src/xml_codec/mxGraphXmlDiagram.ts
+++ src/xml_codec/mxGraphXmlDiagram.ts
@@ -4,36 +4,7 @@ let mxGraph = mxgraph.mxGraph,
     mxCodec = mxgraph.mxCodec,
     mxConstants = mxgraph.mxConstants,
     mxEdgeStyle = mxgraph.mxEdgeStyle,
-    mxUtils = mxgraph.mxUtils,
-    mxCodecRegistry = mxgraph.mxCodecRegistry;
-
-/**
- * The mxgraph library expect to have all of the necessary function directly on window.
- * Instead, we do not want to pollute the window object so we use the mxgraph namespace
- * This replaces the decode function of mxCoded to one that uses mxgraph instead of window
- */
-mxCodec.prototype.decode = function (node, into) {
-    var obj = null;
-    if (node != null && node.nodeType == mxConstants.NODETYPE_ELEMENT) {
-        var ctor = null;
-        try {
-            ctor = mxgraph[node.nodeName];
-        }
-        catch (err) {
-            // ignore
-        }
-        var dec = mxCodecRegistry.getCodec(ctor);
-        if (dec != null) {
-            obj = dec.decode(this, node, into);
-        }
-        else {
-            obj = node.cloneNode(true);
-            obj.removeAttribute('as');
-        }
-    }
-
-    return obj;
-};
+    mxUtils = mxgraph.mxUtils;
 
 export function createGraphFromXML(container, data) {
     let xmlData = mxUtils.parseXml(data);
